20e63c8fdf756daefb8538aa93b0e6b9a9945812
aryanthakur0600 <66875805+aryanthakur0600@users.noreply.github.com>
2023-08-15 14:52:27 +0530

    Update blink_only_3_times.ino

--- blink_only_3_times.ino
+++ blink_only_3_times.ino
@@ -2,57 +2,14 @@ void setup() {
   // put your setup code here, to run once:
 pinMode(12, OUTPUT);
 }
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+// put your main code here, to run repeatedly:
+  while(true)
+  {
   digitalWrite(12, HIGH);    
   delay(1000);                     
   digitalWrite(12, LOW);   
   delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);                     
-  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  
-   digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);  digitalWrite(12, LOW);   
-  delay(1000);
-   digitalWrite(12, HIGH);    
-  delay(1000);                    
-//  digitalWrite(12, LOW);   
-//  delay(1000);
-// 
-//  while(true)
-//  {
-//  
-//   }
+   }
 }
